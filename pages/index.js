aa4138553a2d9b2f0938b0e8700b8ced85e0fe63
fr-me-jim <fr.me.jim@gmail.com>
2019-08-16 18:08:00 +0200

    api response added to props

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Master from '../components/Master';
+import axios from 'axios';
 
-const Index = () => (
+const Index = (props) => (
     <Master>
         <div className="row">
             <div className="col-12">
@@ -14,8 +15,33 @@ const Index = () => (
             <div className="col-md-4">
                 <h2>Future Bitcoin Events</h2>
             </div>
+            {console.log(props)}
         </div>
     </Master>
-)
+);
+
+Index.getInitialProps = async () => {
+    const url = `https://pro-api.coinmarketcap.com/v1/cryptocurrency/quotes/latest?symbol=BTC`;
+
+    const reqOptions = {
+        headers: {
+            "X-CMC_PRO_API_KEY": "b181ee87-a790-4eae-871b-64ce761b21b9",
+            "Accept-Encoding": "deflate"
+        },
+        gzip: true
+    }
+
+    const response = await axios.get(url, reqOptions);
+    // try {
+        
+    //     console.log(response);
+    // } catch (error) {
+    //     console.log(error);
+    // }
+
+    return {
+        bitcoinPrice: response.data
+    }
+}
 
 export default Index;
